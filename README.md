f63dc86b12b5e5a6ec3d72e0310beca5ad0649e0
sam bacha <sam@manifoldfinance.com>
2022-03-06 07:04:48 -0800

    chore(fmt): prettier

--- README.md
+++ README.md
@@ -4,4 +4,8 @@
 
 ## Overview
 
-Error boundaries are React components that catch JavaScript errors anywhere in their child component tree, log those errors, and display a fallback UI instead of the component tree that crashed. Error boundaries catch errors during rendering, in lifecycle methods, and in constructors of the whole tree below them.
+Error boundaries are React components that catch JavaScript errors anywhere in
+their child component tree, log those errors, and display a fallback UI instead
+of the component tree that crashed. Error boundaries catch errors during
+rendering, in lifecycle methods, and in constructors of the whole tree below
+them.
--- src/userAgent.ts
+++ src/userAgent.ts
@@ -1,13 +1,13 @@
 /**
-* @const UAParser
-* @exports const userAgent
-* @exports const isMobile
-*/
-import { UAParser } from 'ua-parser-js'
+ * @const UAParser
+ * @exports const userAgent
+ * @exports const isMobile
+ */
+import { UAParser } from 'ua-parser-js';
 
-const parser = new UAParser(window.navigator.userAgent)
-const { type } = parser.getDevice()
+const parser = new UAParser(window.navigator.userAgent);
+const { type } = parser.getDevice();
 
-export const userAgent = parser.getResult()
+export const userAgent = parser.getResult();
 
-export const isMobile = type === 'mobile' || type === 'tablet'
+export const isMobile = type === 'mobile' || type === 'tablet';
