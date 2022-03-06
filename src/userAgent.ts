c3fc47aa6d14260431cc84e7c81f026e1e92f677
sam bacha <sam@manifoldfinance.com>
2022-03-06 07:04:29 -0800

    feat(data): userAgent

---
+++ src/userAgent.ts
@@ -0,0 +1,13 @@
+/**
+* @const UAParser
+* @exports const userAgent
+* @exports const isMobile
+*/
+import { UAParser } from 'ua-parser-js'
+
+const parser = new UAParser(window.navigator.userAgent)
+const { type } = parser.getDevice()
+
+export const userAgent = parser.getResult()
+
+export const isMobile = type === 'mobile' || type === 'tablet'
