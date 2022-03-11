fe2f896112862c6acdab2e159d2c9cca4c7d74ca
sam bacha <sam@manifoldfinance.com>
2022-03-11 04:34:14 -0800

    Create ErrorBoundary.tsx

---
+++ boundry/ErrorBoundary.tsx
@@ -0,0 +1,19 @@
+/**
+* @export ErrorBoundary
+* @return {error.message}
+*/
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
+  return (
+    <Document title="Uh-oh!">
+      <h1>App Error</h1>
+      <pre>{error.message}</pre>
+      <p>
+        Replace this UI with what you want users to see when your app throws
+        uncaught errors.
+      </p>
+    </Document>
+  );
+};
